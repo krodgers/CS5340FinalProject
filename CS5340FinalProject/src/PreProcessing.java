1df5214766e2aef711de24ac07e224f498f17b57
kathryn <krjrodgers@yahoo.com>
2012-10-22 21:57:13 -0600

    Preprocessing Method Stub

--- CS5340FinalProject/src/PreProcessing.java
+++ CS5340FinalProject/src/PreProcessing.java
@@ -1,4 +1,15 @@
-
+/**
+ * Methods to for preprocessing input files
+ * 
+ * @author Kathryn Rodgers & James Mitchell
+ *
+ */
 public class PreProcessing {
+	
+	public static void stripXML()
+	{
+		
+	}
+	
 
 }
